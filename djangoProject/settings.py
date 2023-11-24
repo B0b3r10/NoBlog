5e926345ca4eecd1ac270c25e322e37d14e9f30d
Robert <robbtish@gmail.com>
2023-11-25 00:27:44 +0300

    final 1 without docker

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import djongo
 
@@ -27,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djongo',
-    'blog'
+    'myblog'
 ]
 
 MIDDLEWARE = [
@@ -111,5 +112,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
