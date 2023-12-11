514b578492d0a94d48a3fb5d51b6716131f65d51
Robert <robbtish@gmail.com>
2023-12-11 18:44:21 +0300

    drop down не работает

--- myblog/forms.py
+++ myblog/forms.py
@@ -3,18 +3,22 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
-class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+# class RegisterUserForm(UserCreationForm):
+#     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+#     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+#     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+#
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2')
+#
+#
+# class LoginUserForm(AuthenticationForm):
+#     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
 
-
-class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-
+class LoginForm1(AuthenticationForm):
+    username = forms.CharField(label="Имя пользователя")
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
--- myblog/views.py
+++ myblog/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.views import LoginView
 from django.views.generic import ListView, TemplateView, CreateView
 from .models import *
 from django.urls import reverse_lazy
-from django.contrib.auth import logout, login
-from django.shortcuts import redirect
+from django.contrib.auth import logout, login, authenticate
+from django.shortcuts import redirect, render
 from .forms import *
 
 
@@ -20,35 +20,50 @@ class InputPageView(TemplateView):
     template_name = 'imput.html'
 
 
-class RegisterUser(CreateView):
-    form_class = RegisterUserForm
-    template_name = 'register.html'
-    success_url = reverse_lazy('login')
+# class RegisterUser(CreateView):
+#     form_class = RegisterUserForm
+#     template_name = 'register.html'
+#     success_url = reverse_lazy('login')
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = "Регистрация"
+#         return context
+#
+#     def form_valid(self, form):
+#         user = form.save()
+#         login(self.request, user)
+#         return redirect('home')
+#
+#
+# class LoginUser(LoginView):
+#     form_class = LoginUserForm
+#     template_name = 'login.html'
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = "Авторизация"
+#         return context
+#
+#     def get_success_url(self):
+#         return reverse_lazy('home')
+#
+#
+# def logout_user(request):
+#     logout(request)
+#     return redirect('login')
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = "Регистрация"
-        return context
 
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return redirect('home')
-
-
-class LoginUser(LoginView):
-    form_class = LoginUserForm
-    template_name = 'login.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = "Авторизация"
-        return context
-
-    def get_success_url(self):
-        return reverse_lazy('home')
-
-
-def logout_user(request):
-    logout(request)
-    return redirect('login')
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm1(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Замените 'home' на вашу домашнюю страницу
+    else:
+        form = LoginForm1(request)
+    return render(request, 'login.html', {'form': form})
