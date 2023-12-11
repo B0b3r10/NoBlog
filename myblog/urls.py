fc8abca82640909a87eb84673f6644f26ae5c58e
Robert <robbtish@gmail.com>
2023-12-11 21:39:21 +0300

    added pagination

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('imput/', InputPageView.as_view(), name='imput'),
-    # path('login/', LoginUser.as_view(), name='login'),
-    # path('logout/', logout_user, name='logout'),
-    # path('register/', RegisterUser.as_view(), name='register'),
-    path('login/', login_view, name='login')
     ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,13 +1,11 @@
-from django.contrib.auth.views import LoginView
-from django.views.generic import ListView, TemplateView, CreateView
-from .models import *
-from django.urls import reverse_lazy
-from django.contrib.auth import logout, login, authenticate
-from django.shortcuts import redirect, render
-from .forms import *
+from django.views.generic import ListView, TemplateView
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 
 
 class BlogListView(ListView):
+    paginate_by = 3
     model = Post
     template_name = 'home.html'
 
@@ -20,50 +18,3 @@ class InputPageView(TemplateView):
     template_name = 'imput.html'
 
 
-# class RegisterUser(CreateView):
-#     form_class = RegisterUserForm
-#     template_name = 'register.html'
-#     success_url = reverse_lazy('login')
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['title'] = "Регистрация"
-#         return context
-#
-#     def form_valid(self, form):
-#         user = form.save()
-#         login(self.request, user)
-#         return redirect('home')
-#
-#
-# class LoginUser(LoginView):
-#     form_class = LoginUserForm
-#     template_name = 'login.html'
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['title'] = "Авторизация"
-#         return context
-#
-#     def get_success_url(self):
-#         return reverse_lazy('home')
-#
-#
-# def logout_user(request):
-#     logout(request)
-#     return redirect('login')
-
-
-def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm1(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')  # Замените 'home' на вашу домашнюю страницу
-    else:
-        form = LoginForm1(request)
-    return render(request, 'login.html', {'form': form})
